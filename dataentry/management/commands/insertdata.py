b39f2e7724c87cf4a9cfc3fe5c3fb7b920b69a3f
Sabbha20 <sabbha.power1@gmail.com>
2024-09-08 09:12:02 +0530

    Custom Command: insertdata modified - validation for duplicate data

--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -10,14 +10,18 @@ class Command(BaseCommand):
         dataset = [
             {'roll_num': 1003, 'name': 'Stephan', 'age':153},
             {'roll_num': 1004, 'name': 'Klaus', 'age':1058 },
-            {'roll_num': 1005, 'name': 'Bart', 'age':43},
-            {'roll_num': 1006, 'name': 'Lisa', 'age':36},
-            {'roll_num': 1007, 'name': 'Homer', 'age':45},
-            {'roll_num': 1008, 'name': 'Marge', 'age':38},
             {'roll_num': 1009, 'name': 'Maggie', 'age':22},
             {'roll_num': 1010, 'name': 'Apu', 'age':39},
+            {'roll_num': 1011, 'name': 'Mike', 'age':19},
+            {'roll_num': 1012, 'name': 'Emma', 'age':29},
+            {'roll_num': 1013, 'name': 'Bonnie', 'age':17}, 
         ]
         
         for data in dataset:
-            Student.objects.create(**data)
+            roll_num = data['roll_num']
+            existing_student = Student.objects.filter(roll_num=roll_num).exists()
+            if not existing_student:
+                Student.objects.create(**data)
+            else:
+                self.stdout.write(self.style.ERROR(f"Student with Roll no.:{roll_num} already exists!"))
         self.stdout.write(self.style.SUCCESS("Data inserted successfully!"))
