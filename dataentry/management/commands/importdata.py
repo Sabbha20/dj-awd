52d4de7fcaf5126af38c4cd1c697d4e5bab427bc
Sabbha20 <sabbha.power1@gmail.com>
2024-10-02 12:38:49 +0530

    Import data from csv to any model

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,18 +1,42 @@
-from django.core.management.base import BaseCommand
-from dataentry.models import Student
+from django.core.management.base import BaseCommand, CommandError
+# from dataentry.models import Student
+from django.apps import apps # Importing all apps
 import csv
+
+# Proposed command - python manage.py importdata <file_path> <model_name>
 
 class Command(BaseCommand):
     help = 'Import data from CSV file'
     
     def add_arguments(self, parser):
         parser.add_argument('file_path', type=str, help="Path to CSV file")
+        parser.add_argument('model_name', type=str, help="Name of the model")
         
     def handle(self, *args, **kwargs):
         file_path = kwargs['file_path']
-        with  open(file_path, 'r') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                Student.objects.create(**row)
-                # print(row)
-        self.stdout.write(self.style.SUCCESS("Data imported successfully!"))
+        model_name = kwargs['model_name'].capitalize()
+        
+        # Search for the models accross all apps
+        model = None
+        for app_config in apps.get_app_configs():
+            # print(app_config)
+            # for model_name in app_config.get_models():
+            #     print("  - ",model_name)
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break # Once we get model, will stop searching for it
+            except LookupError:
+                continue # Will continue, till we get our model
+            
+        if not model:
+            raise CommandError(f"Model: {model_name} not found in any apps!")
+        
+        try:
+            with  open(file_path, 'r') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    model.objects.create(**row)
+                    # print(row)
+            self.stdout.write(self.style.SUCCESS("Data imported successfully!"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR("'File Path' & 'Model name' mismatch!!!"))
--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -8,13 +8,19 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Insert data into database
         dataset = [
-            {'roll_num': 1003, 'name': 'Stephan', 'age':153},
-            {'roll_num': 1004, 'name': 'Klaus', 'age':1058 },
-            {'roll_num': 1009, 'name': 'Maggie', 'age':22},
-            {'roll_num': 1010, 'name': 'Apu', 'age':39},
-            {'roll_num': 1011, 'name': 'Mike', 'age':19},
-            {'roll_num': 1012, 'name': 'Emma', 'age':29},
-            {'roll_num': 1013, 'name': 'Bonnie', 'age':17}, 
+            {'roll_num': 1001, 'name': 'Sabbha Mondal', 'age':10053},
+            {'roll_num': 1002, 'name': 'Andrea Marshall', 'age':273},
+            {'roll_num': 1003, 'name': 'Stephan Salvatore', 'age':153},
+            {'roll_num': 1004, 'name': 'NiKlaus Micheleson', 'age':1058 },
+            {'roll_num': 1005, 'name': 'Damon Salvatore', 'age':163},
+            {'roll_num': 1006, 'name': 'Elaiza Micheleson', 'age':1068 },
+            {'roll_num': 1007, 'name': 'Ribekka Micheleson', 'age':1018 },
+            {'roll_num': 1008, 'name': 'Jerr Gilbert', 'age':22},
+            {'roll_num': 1009, 'name': 'Maggie Robert', 'age':22},
+            {'roll_num': 1010, 'name': 'Apu Ghar', 'age':39},
+            {'roll_num': 1011, 'name': 'Mike Tyson', 'age':19},
+            {'roll_num': 1012, 'name': 'Emma Bennet', 'age':29},
+            {'roll_num': 1013, 'name': 'Bonnie Lawrence', 'age':17}, 
         ]
         
         for data in dataset:
